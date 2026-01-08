09c6a7684c60a352a8d4425eb8f411352f2e50a9
Cristian Contreras <contreras.cristian994@gmail.com>
2026-01-08 01:41:48 -0500

    [convex] clound backend implemented

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,20 @@
 import { ThemeProvider } from "@/hooks/useTheme";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
+console.log("window is:", typeof window);
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </ThemeProvider>
-  ) 
+    <ConvexProvider client={convex}>
+      <ThemeProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </ThemeProvider>
+    </ConvexProvider>
+  );
 }
