f6bf253eb5b81e3d47e15f0b6f6896a63d050998
Cristian Contreras <contreras.cristian994@gmail.com>
2026-01-07 22:19:47 -0500

    [tabs] nab bar implemented

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>npx expo start --host tunnel</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightblue"
+  },
+})
